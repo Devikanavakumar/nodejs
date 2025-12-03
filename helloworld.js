2cd4f3bae1dd37b5ce5434d24ac675a03f52a904
Devikanavakumar <devikanavakumar@gmail.com>
2025-12-03 20:50:03 +0530

    Add complete implementation of basic arithmetic operations and control flow in helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,23 @@
+console.log("Hello,world!");
+var a=10;
+console.log(a);
+var b=10;
+console.log(`sum of ${a} and ${b} is:${a+b}`);
+console.log(`difference of ${b} and ${a} is:${b-a}`);
+console.log(`product of ${a} and ${b} is:${a*b}`);
+console.log(`division of ${b} by ${a} is:${b/a}`);
+if(a>5){
+    console.log(`hello`);
+
+}
+else{
+    console.log(`hi`);
+}
+for(i=0;i<5;i++){
+    for(j=0;j<=i;j++){
+        console.log(i+" "+j);
+    }
+}
+for(i=0;i<=5;i++){
+    console.log(i);
+}
